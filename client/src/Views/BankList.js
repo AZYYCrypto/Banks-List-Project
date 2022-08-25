2166b3654ee8b8477e6e36bee37978285556c585
Dimitar <wopz1337@gmail.com>
2022-08-25 23:10:47 +0300

    Fetch Data from API

--- client/src/Views/BankList.js
+++ client/src/Views/BankList.js
@@ -1,3 +1,27 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 export const BankList = () => {
-  return <div></div>;
+  const [banks, setBanks] = useState();
+
+  const banksUrl =
+    "https://europe-west1-proto-rn-frbs-4242.cloudfunctions.net/loadBranches";
+
+  useEffect(() => {
+    fetch(banksUrl)
+      .then((res) => res.json())
+      .then((res) => setBanks(res.data[0].Brand[0].Branch));
+  }, []);
+
+  return (
+    <div className="BankList">
+      {banks &&
+        banks.map((el, key) => (
+          <div key={key}>
+            <Link to={`/bank-details/${el.Identification}`} state={{ el }}>
+              <div>{el.Name}</div>
+            </Link>
+          </div>
+        ))}
+    </div>
+  );
 };
