95943f1392526aa06692322f6a13d54292efc263
Dimitar <wopz1337@gmail.com>
2022-08-26 21:22:25 +0300

    rename folder

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BankDetails } from "./views/BankDetails";
 import { Header } from "./components/Header/Header";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+
 export default function App() {
   return (
     <div className="App">
